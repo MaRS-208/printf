9cf44f6731272a9b087d0313d1d80d4e9ca5b403
Manu <manuela.riossosa@gmail.com>
2021-11-08 11:58:05 -0800

    commit

--- print_int.c
+++ print_int.c
@@ -7,18 +7,20 @@
 int print_int(int n)
 {
 	int b = 0;
+	unsigned int a = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		return (1 + print_int(n * -1));
+		a = -n;
+		return (1 + print_int(a));
 	}
-	if (n < 10)
+	if (a < 10)
 	{
-		_putchar(n + '0');
+		_putchar(a + '0');
 		return (1);
 	}
-	b = (1 + print_int(n / 10));
-	_putchar((n % 10) + '0');
+	b = (1 + print_int(a / 10));
+	_putchar((a % 10) + '0');
 	return (b);
 }
