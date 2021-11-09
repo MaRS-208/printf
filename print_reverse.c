6cb1fad1555c3154face97af59242666355129ba
Guzman Caraballo <guzchelsea24@gmail.com>
2021-11-09 09:01:32 -0800

    upd print_rev

--- print_reverse.c
+++ print_reverse.c
@@ -1,10 +1,26 @@
 #include "main.h"
+
 /**
- * print_reverse - prints string in reverse
- * @s: string
- * Return: s
- */
+ * print_reverse - print w putchar
+ * @s: pointer
+ * Return: cont
+ **/
+
 int print_reverse(char *s)
 {
-	return (*s);
+	int i, cont;
+
+	i = 0;
+	cont = 0;
+
+	while (*(s + i) != 0)
+		i++;
+	while (i)
+	{
+		i--;
+		_putchar (*(s + i));
+		cont++;
+	}
+	_putchar('\n');
+	return (cont);
 }
